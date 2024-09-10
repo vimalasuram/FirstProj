cfa19e76ae80d5ac4ca2f1da7097f3cc6b885519
vimalasuram <suram.v@northeastern.edu>
2024-09-10 14:24:37 -0400

    Updated Line

--- Lab 0/src/Model/HelloWorld.java
+++ Lab 0/src/Model/HelloWorld.java
@@ -6,6 +6,7 @@ package Model;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
     
 }
