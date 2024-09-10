af8e975dd54199d28ce4b39ab9096166346676f4
vimalasuram <suram.v@northeastern.edu>
2024-09-10 14:19:22 -0400

    Updated

---
+++ Lab 0/src/Model/HelloWorld.java
@@ -0,0 +1,11 @@
+package Model;
+
+/*
+@author  : Vimala_Suram
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+    
+}
